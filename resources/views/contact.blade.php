28382a3c35d59638db33e3c319532ad0de2523c8
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-06-03 13:31:39 -0300

    styles: better spacement

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
             <div class="card-body items-center">
                 <h2 class="card-title">Contact Me</h2>
                 <p>Have a sugestion, issue or simply want to send a message? Just fill above and let's chat!</p>
-                <form wire:submit="sendMessage" class="w-full flex flex-col gap-2 items-center">
+                <form wire:submit="sendMessage" class="w-full flex flex-col gap-4 items-center">
                     <div class="w-full">
                         <label class="input input-bordered flex items-center gap-2">
                             <i class="fa-solid fa-signature"></i>
@@ -32,7 +32,7 @@
                         </label>
                         @error('message') <span class="text-sm text-error">{{ $message }}</span> @enderror
                     </div>
-                    <div class="card-actions justify-center mt-4">
+                    <div class="card-actions justify-center">
                         <button type="submit" autofocus class="btn btn-primary">
                             <div wire:loading.remove>
                                 <i class="fa-regular fa-paper-plane"></i>
