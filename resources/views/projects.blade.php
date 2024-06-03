94fdf808034fd68fb776a91fe8810818b0301108
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-06-03 14:39:54 -0300

    feat: projects style

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,1 +1,65 @@
-<h1 class="text-xl text-white font-bold">Projects!</h1>
+<div class="p-4 h-full min-h-screen w-full flex flex-col items-center justify-center gap-4 bg-home-hero bg-cover bg-center">
+    <article class="prose lg:prose-xl text-center">
+        <h1>
+            Check here
+            <br>
+            Some of my <span class="text-primary">projects</span>
+        </h1>
+    </article>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
+        <div class="card w-full bg-base-200 shadow-xl">
+            <div class="card-body h-80">
+                <strong class="card-title">Project title</strong>
+                <i>Start - End</i>
+                <span>Associated with <b class="text-accent">Associate</b></span>
+                <br>
+                <p class="overflow-y-scroll">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laboriosam nihil earum fugit repudiandae eos in sapiente? Id ipsa illum deleniti. Nihil perspiciatis ipsam expedita quia. Animi, fugit nam. Porro? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, maiores autem? Dicta labore omnis at reiciendis asperiores repudiandae quo nulla, iure deleniti eligendi architecto amet quasi dignissimos officiis, praesentium voluptatem.</p>
+            </div>
+        </div>
+        <div class="card w-full bg-base-200 shadow-xl">
+            <div class="card-body h-80">
+                <strong class="card-title">Project title</strong>
+                <i>Start - End</i>
+                <span>Associated with <b class="text-accent">Associate</b></span>
+                <br>
+                <p class="overflow-y-scroll">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laboriosam nihil earum fugit repudiandae eos in sapiente? Id ipsa illum deleniti. Nihil perspiciatis ipsam expedita quia. Animi, fugit nam. Porro? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, maiores autem? Dicta labore omnis at reiciendis asperiores repudiandae quo nulla, iure deleniti eligendi architecto amet quasi dignissimos officiis, praesentium voluptatem.</p>
+            </div>
+        </div>
+        <div class="card w-full bg-base-200 shadow-xl">
+            <div class="card-body h-80">
+                <strong class="card-title">Project title</strong>
+                <i>Start - End</i>
+                <span>Associated with <b class="text-accent">Associate</b></span>
+                <br>
+                <p class="overflow-y-scroll">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laboriosam nihil earum fugit repudiandae eos in sapiente? Id ipsa illum deleniti. Nihil perspiciatis ipsam expedita quia. Animi, fugit nam. Porro? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, maiores autem? Dicta labore omnis at reiciendis asperiores repudiandae quo nulla, iure deleniti eligendi architecto amet quasi dignissimos officiis, praesentium voluptatem.</p>
+            </div>
+        </div>
+        <div class="card w-full bg-base-200 shadow-xl">
+            <div class="card-body h-80">
+                <strong class="card-title">Project title</strong>
+                <i>Start - End</i>
+                <span>Associated with <b class="text-accent">Associate</b></span>
+                <br>
+                <p class="overflow-y-scroll">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laboriosam nihil earum fugit repudiandae eos in sapiente? Id ipsa illum deleniti. Nihil perspiciatis ipsam expedita quia. Animi, fugit nam. Porro? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, maiores autem? Dicta labore omnis at reiciendis asperiores repudiandae quo nulla, iure deleniti eligendi architecto amet quasi dignissimos officiis, praesentium voluptatem.</p>
+            </div>
+        </div>
+        <div class="card w-full bg-base-200 shadow-xl">
+            <div class="card-body h-80">
+                <strong class="card-title">Project title</strong>
+                <i>Start - End</i>
+                <span>Associated with <b class="text-accent">Associate</b></span>
+                <br>
+                <p class="overflow-y-scroll">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laboriosam nihil earum fugit repudiandae eos in sapiente? Id ipsa illum deleniti. Nihil perspiciatis ipsam expedita quia. Animi, fugit nam. Porro? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, maiores autem? Dicta labore omnis at reiciendis asperiores repudiandae quo nulla, iure deleniti eligendi architecto amet quasi dignissimos officiis, praesentium voluptatem.</p>
+            </div>
+        </div>
+        <div class="card w-full bg-base-200 shadow-xl">
+            <div class="card-body h-80">
+                <strong class="card-title">Project title</strong>
+                <i>Start - End</i>
+                <span>Associated with <b class="text-accent">Associate</b></span>
+                <br>
+                <p class="overflow-y-scroll">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laboriosam nihil earum fugit repudiandae eos in sapiente? Id ipsa illum deleniti. Nihil perspiciatis ipsam expedita quia. Animi, fugit nam. Porro? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, maiores autem? Dicta labore omnis at reiciendis asperiores repudiandae quo nulla, iure deleniti eligendi architecto amet quasi dignissimos officiis, praesentium voluptatem.</p>
+            </div>
+        </div>
+    </div>
+</div>
