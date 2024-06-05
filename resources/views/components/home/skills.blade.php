b6a2469ffda6623c04d7e995b5531f59a74bf433
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-06-05 11:51:32 -0300

    feat: add a new element in skills component

--- resources/views/components/home/skills.blade.php
+++ resources/views/components/home/skills.blade.php
@@ -1,13 +1,22 @@
 <div class="w-full bg-base-300 px-4 py-12 flex justify-center items-center">
     <div class="container flex flex-col md:flex-row items-center gap-4">
-        <article class="prose lg:prose-xl">
-            <h2>
-                <span class="text-primary">Skills</span> and tools
-            </h2>
-            <p>
-                For a more <span class="text-primary">detailed</span> overview, please feel free to check the tools that were used on a per project basis
-            </p>
-        </article>
+        <div class="flex flex-col gap-4">
+            <article class="prose lg:prose-xl">
+                <h2>
+                    <span class="text-primary">Skills</span> and tools
+                </h2>
+                <p>
+                    For a more <span class="text-primary">detailed</span> overview, please feel free to check the tools that were used on a per project basis
+                </p>
+            </article>
+            <div class="mockup-code">
+                <pre data-prefix="$"><code>ls gabe/interests -lh | head -3</code></pre>
+                <pre class="text-primary"><code>total &#8734;</code></pre>
+                <pre data-prefix=">" class="text-secondary"><code>Artificial Intelligence</code></pre>
+                <pre data-prefix=">" class="text-secondary"><code>Blockchain and Web3</code></pre>
+                <pre data-prefix=">" class="text-secondary"><code>Software Development</code></pre>
+              </div>
+        </div>
         <article class="prose lg:prose-xl">
             <img src="https://skillicons.dev/icons?i=html,css,less,sass,js,ts,php,java,python,go,solidity,bash" alt="Programming languages" class="pb-1" />
             <img src="https://skillicons.dev/icons?i=tailwind,react,next,vue,nuxt,vitest,jest,express,nestjs,adonis,angular,laravel" alt="Frameworks and libraries" class="pb-1" />
