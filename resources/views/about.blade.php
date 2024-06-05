b56c82577943f5fc9a32bbbc0a74713c498cf341
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-06-05 12:01:04 -0300

    feat: about layout adjust and lint

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,35 +1,53 @@
 <div class="flex flex-col items-center">
-    <div class="relative p-4 h-full min-h-screen w-full flex flex-col md:flex-row items-center justify-center gap-4 bg-home-hero bg-cover bg-center">
-        <div class="w-full md:w-1/2">
+    <div
+        class="relative p-4 h-full min-h-screen w-full flex flex-col md:flex-row items-center justify-center gap-4 bg-home-hero bg-cover bg-center">
+        <div class="w-full md:w-1/2 flex flex-col gap-4">
             <article class="prose lg:prose-xl text-center">
                 <div class="avatar">
-                    <figure class="w-60 h-60 relative rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 rounded-full">
+                    <figure
+                        class="w-60 h-60 relative rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 rounded-full">
                         <span class="absolute top-0 right-16 w-4 h-4 rounded-full bg-success animate-pulse"></span>
-                        <img src="https://pt.gravatar.com/userimage/199484219/012c5298d952c34d6699a3d280e3a4f3.png?size=350" class="rounded-full" />
+                        <img src="https://pt.gravatar.com/userimage/199484219/012c5298d952c34d6699a3d280e3a4f3.png?size=350"
+                            class="rounded-full" />
                     </figure>
                 </div>
                 <h1>
                     Gabriel Santos Cardoso
                 </h1>
-                <p>Full Stack Developer, interested in: <span class="text-accent">web applications, blockchain and artificial intelligence</span></p>
             </article>
+            <div class="mockup-code">
+                <pre data-prefix="$"><code>info Gabe | head</code></pre>
+                <pre class="text-secondary"><code>File: Gabe.info, Node: Invoking find, Next: Invoking locate, Up: Reference</code></pre>
+                <pre class="text-secondary"><code>1.1 Invoking 'Gabe'</code></pre>
+                <pre class="text-secondary"><code>===================</code></pre>
+                <pre class="text-secondary"><code>'Gabe', Full Stack Developer, located in Brazil.</code></pre>
+                <pre class="text-secondary"><code>Interests: AI, Blockchain, Web3 and disruptive technologies.</code></pre>
+            </div>
         </div>
         <div class="card w-full md:w-1/2 bg-base-100 shadow-xl">
             <div class="card-body items-center">
                 <h2 class="card-title">About Me</h2>
                 <article class="prose lg:prose-xl">
                     <p>
-                        Hello, I'm Gabriel Santos Cardoso. I currently reside in Brazil, and always open to new opportunities. Feel free to contact me to discuss business, and studies, or simply exchange a good idea between colleagues.
+                        Hello, I'm Gabriel Santos Cardoso. I currently reside in Brazil, and always open to new
+                        opportunities. Feel free to contact me to discuss business, and studies, or simply exchange a
+                        good idea between colleagues.
                     </p>
                     <p>
-                        As a seasoned Full Stack developer  I am passionate about creating innovative solutions that drive business success. With a proven track record of developing scalable and successful web applications, I am skilled in both front-end and back-end technologies, including modern frameworks and multicultural environments.
+                        As a seasoned Full Stack developer I am passionate about creating innovative solutions that
+                        drive business success. With a proven track record of developing scalable and successful web
+                        applications, I am skilled in both front-end and back-end technologies, including modern
+                        frameworks and multicultural environments.
                     </p>
                     <p>
-                        My experience includes designing and implementing software, deploying applications, and optimizing performance through responsive web design. I am proficient in version control systems such as Git, and have a strong ability to troubleshoot and debug applications.
+                        My experience includes designing and implementing software, deploying applications, and
+                        optimizing performance through responsive web design. I am proficient in version control systems
+                        such as Git, and have a strong ability to troubleshoot and debug applications.
                     </p>
                 </article>
                 <div class="card-actions justify-end">
-                    <button type="button" autofocus class="btn btn-primary" wire:click="redirectToContact">Let's get in touch!</button>
+                    <button type="button" autofocus class="btn btn-primary" wire:click="redirectToContact">Let's get in
+                        touch!</button>
                 </div>
             </div>
         </div>
--- resources/views/components/home/skills.blade.php
+++ resources/views/components/home/skills.blade.php
@@ -6,7 +6,8 @@
                     <span class="text-primary">Skills</span> and tools
                 </h2>
                 <p>
-                    For a more <span class="text-primary">detailed</span> overview, please feel free to check the tools that were used on a per project basis
+                    For a more <span class="text-primary">detailed</span> overview, please feel free to check the tools
+                    that were used on a per project basis
                 </p>
             </article>
             <div class="mockup-code">
@@ -15,13 +16,17 @@
                 <pre data-prefix=">" class="text-secondary"><code>Artificial Intelligence</code></pre>
                 <pre data-prefix=">" class="text-secondary"><code>Blockchain and Web3</code></pre>
                 <pre data-prefix=">" class="text-secondary"><code>Software Development</code></pre>
-              </div>
+            </div>
         </div>
         <article class="prose lg:prose-xl">
-            <img src="https://skillicons.dev/icons?i=html,css,less,sass,js,ts,php,java,python,go,solidity,bash" alt="Programming languages" class="pb-1" />
-            <img src="https://skillicons.dev/icons?i=tailwind,react,next,vue,nuxt,vitest,jest,express,nestjs,adonis,angular,laravel" alt="Frameworks and libraries" class="pb-1" />
-            <img src="https://skillicons.dev/icons?i=mysql,postgresql,mongodb,redis,prisma,firebase,supabase,aws,gcp,vercel,netlify,heroku" alt="Database and ORMs" class="pb-1" />
-            <img src="https://skillicons.dev/icons?i=pnpm,yarn,nodejs,bun,vite,docker,git,linux,github,gitlab,figma,githubactions" alt="Tools set" class="py-1" />
+            <img src="https://skillicons.dev/icons?i=html,css,less,sass,js,ts,php,java,python,go,solidity,bash"
+                alt="Programming languages" class="pb-1" />
+            <img src="https://skillicons.dev/icons?i=tailwind,react,next,vue,nuxt,vitest,jest,express,nestjs,adonis,angular,laravel"
+                alt="Frameworks and libraries" class="pb-1" />
+            <img src="https://skillicons.dev/icons?i=mysql,postgresql,mongodb,redis,prisma,firebase,supabase,aws,gcp,vercel,netlify,heroku"
+                alt="Database and ORMs" class="pb-1" />
+            <img src="https://skillicons.dev/icons?i=pnpm,yarn,nodejs,bun,vite,docker,git,linux,github,gitlab,figma,githubactions"
+                alt="Tools set" class="py-1" />
         </article>
     </div>
 </div>
