0b718986ca0aca7ddc9b208cb0ebf408165dfb79
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-06-05 11:38:28 -0300

    feat: new projects page

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,22 +1,50 @@
-<div class="p-4 h-full min-h-screen w-full flex flex-col items-center justify-center gap-4 bg-home-hero bg-cover bg-center">
-    <article class="prose lg:prose-xl text-center">
-        <h1>
-            Check here
-            <br>
-            Some of my <span class="text-primary">projects</span>
-        </h1>
-    </article>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
-        @foreach ($projects as $project)
-            <div class="card w-full bg-base-200 shadow-xl">
-                <div class="card-body h-80">
-                    <strong class="card-title">{{ $project->title }}</strong>
-                    <i>{{ $project->start_date }} - {{ $project->end_date ?? 'Present' }}</i>
-                    <span>Associated with <b class="text-accent">{{ $project->association }}</b></span>
-                    <br>
-                    <p class="overflow-y-scroll">{{ $project->description }}</p>
+<div class="flex flex-col items-center">
+    <div
+        class="p-4 h-full min-h-screen w-full flex flex-col items-center justify-center gap-4 bg-home-hero bg-cover bg-center">
+        <article class="prose lg:prose-xl text-center">
+            <div class="avatar">
+                <figure
+                    class="w-60 h-60 relative rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 rounded-full">
+                    <span class="absolute top-0 right-16 w-4 h-4 rounded-full bg-success animate-pulse"></span>
+                    <img src="https://pt.gravatar.com/userimage/199484219/012c5298d952c34d6699a3d280e3a4f3.png?size=350"
+                        class="rounded-full" />
+                </figure>
+            </div>
+            <h1>
+                Check here
+                <br>
+                Some of my <span class="text-primary">projects</span>
+            </h1>
+        </article>
+    </div>
+    <div class="w-full flex flex-col gap-4">
+        <div class="p-4 flex flex-col md:flex-row items-center justify-center gap-4">
+            <article class="prose lg:prose-xl">
+                <h2>
+                    <span class="badge badge-accent badge-lg"></span>
+                    Project name
+                </h2>
+                <span class="text-secondary">
+                    Marketing Website Redesign
+                </span>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur ratione inventore, odit
+                    aspernatur possimus architecto nulla cupiditate dolorum ipsum reprehenderit voluptate consequuntur
+                    nesciunt quis cum ex magnam placeat sapiente voluptatum.
+                </p>
+                <a href="#" target="_blank" rel="noopener noreferrer"
+                    class="font-bold no-underline hover:text-secondary transition-colors">
+                    View Project <i class="fa-solid fa-arrow-right text-accent"></i>
+                </a>
+            </article>
+            <div>
+                <div class="mockup-window rounded-lg bg-base-300 max-w-[1024px] w-full">
+                    <figure>
+                        <img src="images/home-hero.webp" alt="Project Overview" class="bg-contain">
+                    </figure>
                 </div>
             </div>
-        @endforeach
+        </div>
+        <div class="divider"></div>
     </div>
 </div>
