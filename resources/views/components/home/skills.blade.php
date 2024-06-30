aa044d449089ab1096beae0b8d6d968167966317
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-06-30 14:56:29 -0300

    feat: adjustments

--- resources/views/components/home/skills.blade.php
+++ resources/views/components/home/skills.blade.php
@@ -11,22 +11,31 @@
                 </p>
             </article>
             <div class="mockup-code">
-                <pre data-prefix="$"><code>ls gabe/interests -lh | head -3</code></pre>
+                <pre data-prefix="$"><code>ls gabe/interests -lh | head -5</code></pre>
                 <pre class="text-primary"><code>total &#8734;</code></pre>
                 <pre data-prefix=">" class="text-secondary"><code>Artificial Intelligence</code></pre>
-                <pre data-prefix=">" class="text-secondary"><code>Blockchain and Web3</code></pre>
+                <pre data-prefix=">" class="text-secondary"><code>Blockchain</code></pre>
+                <pre data-prefix=">" class="text-secondary"><code>Web3</code></pre>
+                <pre data-prefix=">" class="text-secondary"><code>Programming Languages</code></pre>
                 <pre data-prefix=">" class="text-secondary"><code>Software Development</code></pre>
             </div>
         </div>
-        <article class="prose lg:prose-xl">
-            <img src="https://go-skill-icons.vercel.app/icons?i=html,css,less,sass,js,ts,php,java,python,go,solidity,bash"
-                alt="Programming languages" class="pb-1" />
-            <img src="https://go-skill-icons.vercel.app/icons?i=tailwind,react,next,vue,nuxt,vitest,jest,express,nestjs,adonis,angular,laravel"
-                alt="Frameworks and libraries" class="pb-1" />
-            <img src="https://go-skill-icons.vercel.app/icons?i=mysql,postgresql,mongodb,redis,prisma,firebase,supabase,aws,gcp,vercel,netlify,heroku"
-                alt="Database and ORMs" class="pb-1" />
-            <img src="https://go-skill-icons.vercel.app/icons?i=pnpm,yarn,nodejs,bun,vite,docker,git,linux,github,gitlab,figma,githubactions"
-                alt="Tools set" class="py-1" />
+        <article class="prose-sm">
+            <h3>Languages and preprocessors</h3>
+            <img src="https://go-skill-icons.vercel.app/api/icons?i=bash,bootstrap,css,elixir,go,html,java,js,less,php,py,sass,solidity,tailwind,ts"
+                alt="Languages and preprocessors"/>
+            <h3>Frameworks and libs</h3>
+            <img src="https://go-skill-icons.vercel.app/api/icons?i=adonis,angular,astro,alpinejs,filament,jest,laravel,livewire,breeze,cashier,echo,envoyer,forge,inertia,jetstream,nova,pint,prompts,sail,sanctum,socialite,telescope,vapor,nestjs,nodejs,nuxtjs,pinia,vite,vitest,vue"
+                alt="Frameworks and libs"/>
+            <h3>Database/ORMs and Cloud services</h3>
+            <img src="https://go-skill-icons.vercel.app/api/icons?i=aws,digitalocean,firebase,gcp,heroku,mongodb,mysql,netlify,nginx,postgresql,prisma,redis,supabase,sqlite,vercel"
+                alt="Database/ORMs and Cloud Services"/>
+            <h3>Tools and more</h3>
+            <img src="https://go-skill-icons.vercel.app/api/icons?i=dbeaver,discord,docker,figma,gimp,github,githubactions,googleanalytics,insomnia,pnpm,linux,md,npm,vscode,yarn"
+                alt="Tools and more" class="py-1" />
+            <h3>Stu(dying) amd interest</h3>
+            <img src="https://go-skill-icons.vercel.app/api/icons?i=aws,azure,bun,chatgpt,codeigniter,elixir,gcp,gemini,go,js,php,spring,solidity,solidjs,svelte"
+                alt="Stu(dying) and interests" class="py-1" />
         </article>
     </div>
 </div>
