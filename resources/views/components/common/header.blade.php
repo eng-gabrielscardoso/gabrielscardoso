f54210f4b7538759129ef628c558e697614879ce
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-01-30 11:22:40 -0300

    refactor(header): enhance navbar structure and add icons to menu items

--- resources/views/components/common/header.blade.php
+++ resources/views/components/common/header.blade.php
@@ -1,24 +1,36 @@
-<div class="fixed navbar bg-base-100 shadow-md z-50 rounded-b-md">
+<div class="fixed z-50 shadow-md navbar bg-base-100 rounded-b-md">
     <div class="navbar-start">
         <div class="dropdown">
-            <button role="button" class="btn btn-ghost btn-circle cursor-pointer">
+            <button role="button" class="cursor-pointer btn btn-ghost btn-circle">
                 <i class="fa-solid fa-bars"></i>
             </button>
             <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                 <li>
-                    <a href="/about" wire:navigate>About</a>
+                    <a href="/about" wire:navigate>
+                        <i class="fa-solid fa-circle-info"></i> About
+                    </a>
                 </li>
                 <li>
-                    <a href="/projects" wire:navigate>Projects</a>
+                    <a href="/projects" wire:navigate>
+                        <i class="fa-solid fa-diagram-project"></i> Projects
+                    </a>
                 </li>
                 <li>
-                    <a href="/contact" wire:navigate>Contact</a>
+                    <a href="/contact" wire:navigate>
+                        <i class="fa-solid fa-phone"></i> Contact
+                    </a>
+                </li>
+                <li>
+                    <a href="https://docs.google.com/document/d/1k-huyj7Ao5NEy0reBUe9AI8PXiiT-w4Z5NsdiJkEIrg"
+                        target="_blank">
+                        <i class="fa-regular fa-address-card"></i> Curriculum vitae
+                    </a>
                 </li>
             </ul>
         </div>
     </div>
     <div class="navbar-center">
-        <a href="/" class="btn btn-ghost text-xl" wire:navigate>{{ config('app.name') }}</a>
+        <a href="/" class="text-xl btn btn-ghost" wire:navigate>{{ config('app.name') }}</a>
     </div>
     <div class="navbar-end">
         <a href="https://github.com/eng-gabrielscardoso/gabrielscardoso" target="_blank" rel="noopener noreferrer"
