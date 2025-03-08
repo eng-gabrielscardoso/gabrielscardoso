75fdc917f706cc8e938081d09e33b1e03d04d0ef
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-03-08 11:26:49 -0300

    adjust footer

--- resources/views/components/common/footer.blade.php
+++ resources/views/components/common/footer.blade.php
@@ -1,4 +1,5 @@
-<footer class="p-10 footer footer-center bg-base-200 text-base-content rounded-t-md">
+<footer
+    class="flex flex-col gap-2 p-2 md:flex-row md:justify-center footer footer-center bg-base-200 text-base-content rounded-t-md">
     <nav class="grid grid-flow-col gap-4">
         <a href="/about" class="link link-hover">About</a>
         <a href="/projects" class="link link-hover">Projects</a>
@@ -6,13 +7,16 @@
     </nav>
     <nav>
         <div class="grid grid-flow-col gap-4">
-            <a href="https://github.com/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer" class="btn btn-ghost btn-circle">
+            <a href="https://github.com/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer"
+                class="btn btn-ghost btn-circle">
                 <i class="fa-brands fa-github fa-2xl"></i>
             </a>
-            <a href="https://linkedin.com/in/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer" class="btn btn-ghost btn-circle">
+            <a href="https://linkedin.com/in/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer"
+                class="btn btn-ghost btn-circle">
                 <i class="fa-brands fa-linkedin fa-2xl"></i>
             </a>
-            <a href="https://instagram.com/eng.gabrielscardoso" target="_blank" rel="noopener noreferrer" class="btn btn-ghost btn-circle">
+            <a href="https://instagram.com/eng.gabrielscardoso" target="_blank" rel="noopener noreferrer"
+                class="btn btn-ghost btn-circle">
                 <i class="fa-brands fa-instagram fa-2xl"></i>
             </a>
         </div>
@@ -20,7 +24,8 @@
     <aside>
         <span>
             Copyright © {{ date('Y') }} - All right reserved by
-            <a href="https://github.com/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer" class="text-primary link">Gabriel Santos Cardoso</a>
+            <a href="https://github.com/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer"
+                class="text-primary link">Gabriel Santos Cardoso</a>
         </span>
     </aside>
 </footer>
