7fc8df53fafd4198c7d7e444b20407588e592e84
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-05-23 11:31:53 -0300

    fix hierarchy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,21 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    {{-- meta tags --}}
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    {{-- seo helper --}}
-    {!! seo() !!}
-
-    {{-- google fonts imports --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-        rel="stylesheet">
-
     {{-- google tag manager --}}
     <script>(function (w, d, s, l, i) {
             w[l] = w[l] || []; w[l].push({
@@ -35,6 +20,21 @@
             y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
         })(window, document, "clarity", "script", "rnkap2kupt");
     </script>
+
+    {{-- meta tags --}}
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- seo helper --}}
+    {!! seo() !!}
+
+    {{-- google fonts imports --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
 
     {{-- font awesome imports --}}
     <script src="https://kit.fontawesome.com/21370d6dca.js" crossorigin="anonymous"></script>
