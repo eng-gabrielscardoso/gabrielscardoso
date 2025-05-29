bac610a0daab19bbf84927495f16cb7756cc85e1
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-05-29 12:42:53 -0300

    refactor About and Welcome pages to streamline layout and improve component organization

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -39,4 +39,8 @@
             </div>
         </div>
     </div>
+    <div class="container flex flex-col items-center gap-8 px-4 py-12">
+        <livewire:home.educations>
+        <livewire:home.experiences>
+    </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,4 @@
 <div class="flex flex-col items-center">
     <livewire:home.headline>
-        <livewire:home.skills>
-            <div class="container flex flex-col items-center gap-8 px-4 py-12">
-                <livewire:home.educations>
-                    <livewire:home.experiences>
-            </div>
+    <livewire:home.skills>
 </div>
