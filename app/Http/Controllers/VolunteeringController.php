3dd3f38a5ce47e839bdd20052ebaa69099cd18b5
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-05-29 15:25:33 -0300

    add Volunteering resource controller, request classes, policy, and tests

---
+++ app/Http/Controllers/VolunteeringController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVolunteeringRequest;
+use App\Http\Requests\UpdateVolunteeringRequest;
+use App\Models\Volunteering;
+
+class VolunteeringController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVolunteeringRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Volunteering $volunteering)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Volunteering $volunteering)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVolunteeringRequest $request, Volunteering $volunteering)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Volunteering $volunteering)
+    {
+        //
+    }
+}
