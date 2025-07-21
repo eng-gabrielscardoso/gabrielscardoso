fa0c7fb951331141094c510bf2a093f6c756fc0d
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-07-21 10:24:50 -0300

    fix: add aria-label and title attributes for improved accessibility in header, footer, and buy-me-an-acai components

--- resources/views/components/common/footer.blade.php
+++ resources/views/components/common/footer.blade.php
@@ -1,22 +1,26 @@
 <footer
     class="flex flex-col gap-2 p-2 md:flex-row md:justify-center footer footer-center bg-base-200 text-base-content rounded-t-md">
     <nav class="grid grid-flow-col gap-4">
-        <a href="/about" class="link link-hover">About</a>
-        <a href="/projects" class="link link-hover">Projects</a>
-        <a href="/contact" class="link link-hover">Contact</a>
+        <a href="/about" class="link link-hover" aria-label="About Page Link" title="About Page Link">About</a>
+        <a href="/projects" class="link link-hover" aria-label="Projects Page Link"
+            title="Projects Page Link">Projects</a>
+        <a href="/contact" class="link link-hover" aria-label="Contact Page Link" title="Contact Page Link">Contact</a>
     </nav>
     <nav>
         <div class="grid grid-flow-col gap-4">
             <a href="https://github.com/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer"
-                class="btn btn-ghost btn-circle">
+                class="btn btn-ghost btn-circle" aria-label="Developer GitHub Profile Link"
+                title="Developer GitHub Profile Link">
                 <i class="fa-brands fa-github fa-2xl"></i>
             </a>
             <a href="https://linkedin.com/in/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer"
-                class="btn btn-ghost btn-circle">
+                class="btn btn-ghost btn-circle" aria-label="Developer LinkedIn Profile Link"
+                title="Developer LinkedIn Profile Link">
                 <i class="fa-brands fa-linkedin fa-2xl"></i>
             </a>
             <a href="https://instagram.com/eng.gabrielscardoso" target="_blank" rel="noopener noreferrer"
-                class="btn btn-ghost btn-circle">
+                class="btn btn-ghost btn-circle" aria-label="Developer Instagram Profile Link"
+                title="Developer Instagram Profile Link">
                 <i class="fa-brands fa-instagram fa-2xl"></i>
             </a>
         </div>
@@ -25,7 +29,8 @@
         <span>
             Copyright © {{ date('Y') }} - All right reserved by
             <a href="https://github.com/eng-gabrielscardoso" target="_blank" rel="noopener noreferrer"
-                class="text-primary link">Gabriel Santos Cardoso</a>
+                class="text-primary link" aria-label="Developer GitHub Profile Link"
+                title="Developer GitHub Profile Link">Gabriel Santos Cardoso</a>
         </span>
     </aside>
 </footer>
